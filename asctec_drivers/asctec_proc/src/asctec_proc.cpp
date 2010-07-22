0f817becedd406dd89b1700cbfb7596914cc51db
Ivan Dryanovski <ivan.dryanovski@gmail.com>
2010-07-22 14:13:08 -0400

    .

--- asctec_drivers/asctec_proc/src/asctec_proc.cpp
+++ asctec_drivers/asctec_proc/src/asctec_proc.cpp
@@ -100,7 +100,10 @@ void AsctecProc::createImuMsg(const asctec_msgs::IMUCalcDataConstPtr& imuCalcDat
   orientation.setRPY(imuCalcDataMsg->angle_roll * ASC_TO_ROS_ANGLE,
                      imuCalcDataMsg->angle_nick * ASC_TO_ROS_ANGLE,
                      imuCalcDataMsg->angle_yaw  * ASC_TO_ROS_ANGLE);
-  
+
+  printf("AP: ROLL: %f, PITCH: %f, YAW: %f\n", imuCalcDataMsg->angle_roll * ASC_TO_ROS_ANGLE,
+                     imuCalcDataMsg->angle_nick * ASC_TO_ROS_ANGLE,
+                     imuCalcDataMsg->angle_yaw  * ASC_TO_ROS_ANGLE);  
 
 
   imuMsg.orientation.x = orientation.getX();
