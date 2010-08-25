7bd49a8d7dda47d74943fca811d6d2fd7fe291dc
Ivan Dryanovski <ivan.dryanovski@gmail.com>
2010-08-25 10:15:11 -0400

    changed to shell script to static IP

--- asctec_drivers/pelican_launch/scripts/network.strange.sh
+++ asctec_drivers/pelican_launch/scripts/network.strange.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 date
-echo "Exporting ROS_MASTER_URI=http://strange:11311"
+echo "Exporting ROS_MASTER_URI=http://192.168.1.3:11311"
 
-export ROS_MASTER_URI=http://strange:11311
+export ROS_MASTER_URI=http://192.168.1.33:11311
 
 
